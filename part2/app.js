0ec0604245c909e060bab6c83bb639519bea2dbd
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 21:31:31 +0000

    [automatic commit] 2025-06-20 21:31:31 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -124,7 +124,7 @@ app.get('/load_user_dogs', (req, res) => {
         }
         if (results.length > 0) {
             console.log('User dogs:', results);
-            res.status(200).json(results); // Send the list of dogs back
+            return res.status(200).json(results); // Send the list of dogs back
         } else {
             res.status(404).json({ message: 'No dogs found for this user' });
         }
