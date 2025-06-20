eabefce3b2dca3c5670b01594cbe5a44d89f2f80
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 15:41:03 +0000

    [automatic commit] 2025-06-20 15:41:03 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -14,7 +14,7 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'DogWalkService',
+    database: 'DogWalkService.sql',
     connectionLimit: 10 // Adjust as needed
 });
 
