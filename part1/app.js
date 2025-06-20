60fc9aeeb3a57a1c78a94478fa7ce14a05e853a0
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 08:44:05 +0000

    [automatic commit] 2025-06-20 08:44:05 UTC 1.158.152.213

--- part1/app.js
+++ part1/app.js
@@ -52,7 +52,7 @@ app.get('/dogs', async (req, res) => {
 // Route to return walkrequests that are open as JSON
 app.get('/walkrequests/open', async (req, res) => {
     try {
-        const [dogs_data] = await db.execute('SELECT * FROM WalkRequests WHERE status = "open"');
+        const [dogs_data] = await db.execute('SELECT * FROM WalkRequests WHERE status = "open" INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.id');
         res.json(dogs_data);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch data' });
