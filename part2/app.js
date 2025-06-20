490a6d2daac05881b8d32973aee624d1d949d7b5
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 19:22:54 +0000

    [automatic commit] 2025-06-20 19:22:54 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -50,8 +50,8 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-}
+    console.log(req.session.user);
+});
 app.post('/login', (req, res) => {
 
     // SQL query to check if the user exists
