bc183df1af18fd304162438292ab34a429fe10d7
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 21:49:39 +0000

    [automatic commit] 2025-06-20 21:49:39 UTC 1.158.152.213

--- part2/public/test.sql
+++ part2/public/test.sql
@@ -1,3 +1,4 @@
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
-((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'john', 'small'),
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'john', 'small')
