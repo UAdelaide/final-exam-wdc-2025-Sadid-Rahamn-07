039d6cf285174c0d16f9a13e4c525a42a12f182f
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 14:36:40 +0000

    [automatic commit] 2025-06-20 14:36:40 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -6,13 +6,13 @@ require('dotenv').config();
 const app = express();
 
 //database connection
-const mysql = require('mysql');
+
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
