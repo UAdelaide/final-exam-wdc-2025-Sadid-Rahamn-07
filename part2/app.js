b11b63a0a969a860041ac098191415c9b7ed66f6
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 22:09:06 +0000

    [automatic commit] 2025-06-20 22:09:06 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -108,7 +108,7 @@ app.get('/load_user_dogs', (req, res) => {
 
     // SQL query to get all dogs for the user
     const query = `
-        SELECT Dogs.name
+        SELECT Dogs.name, Dogs.dog_id
         FROM Dogs
         INNER JOIN Users ON Dogs.owner_id = Users.user_id
         WHERE Users.username = ?
