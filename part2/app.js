c2aaefb05b496533c869d47065bb3dc4bc06bfa2
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 15:39:43 +0000

    [automatic commit] 2025-06-20 15:39:43 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -14,7 +14,7 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: ''',
     connectionLimit: 10 // Adjust as needed
 });
 
