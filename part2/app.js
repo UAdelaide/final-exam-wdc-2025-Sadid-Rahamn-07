52edda7f93e05403d4e3f84b11d04cb1f54e72a9
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 17:12:20 +0000

    [automatic commit] 2025-06-20 17:12:20 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -76,7 +76,13 @@ app.post('/login', (req, res) => {
 app.get('/dogs', (req, res) => {
     // SQL query to get all dogs
     const dogs_sql = 'SELECT * FROM Dogs';
-    db.query(dogs_sql, ())
+    db.query(dogs_sql, (err, results) => {
+        if (err) {
+            console.error('Database query error:', err);
+            return res.status(500).json({ error: 'Database query error' });
+        }
+        res.status(200).json(results); // Send the list of dogs back
+    });
 })
 
 // Export the app instead of listening here
