7a9d40679135d23db05da03394cdccf5e1f18f82
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 10:40:50 +0000

    [automatic commit] 2025-06-20 10:40:50 UTC 1.158.152.213

--- part1/app.js
+++ part1/app.js
@@ -82,20 +82,16 @@ app.get('/walkrequests/open', async (req, res) => {
 app.get('/walkers/summary', async (req, res) => {
     try {
         const [walkers_summary] = await db.execute(`
-            SELECT
-            u.username AS walker_username,
-            COUNT(DISTINCT r.rating_id) AS total_ratings,
-            ROUND(AVG(r.rating), 2) AS average_rating,
-            COUNT(DISTINCT CASE WHEN wr.status = 'completed' THEN wr.request_id END) AS completed_walks
+        SELECT
+        u.username AS walker_username,
+        COUNT(r.rating) AS total_ratings,
+        AVG(r.rating) AS average_rating,
+        COUNT(CASE WHEN wr.status = 'completed' THEN 1 END) AS completed_walks
         FROM Users u
-        LEFT JOIN WalkApplications wa
-        ON u.user_id = wa.walker_id AND wa.status = 'accepted'
-        LEFT JOIN WalkRequests wr
-        ON wa.request_id = wr.request_id
-        LEFT JOIN WalkRatings r
-        ON u.user_id = r.walker_id
+        LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
+        LEFT JOIN WalkRequests wr ON u.user_id = wr.dog_id -- Not correct: fix below
         WHERE u.role = 'walker'
-        GROUP BY u.user_id, u.username;
+        GROUP BY u.username;
     `);
         res.json(walkers_summary);
     } catch (err) {
