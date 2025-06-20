5fe6842500faad096efda645f67ab7e1024a7655
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 19:12:09 +0000

    [automatic commit] 2025-06-20 19:12:09 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -78,9 +78,14 @@ app.post('/login', (req, res) => {
     });
 });
 
-app.post('/logout',(req, res) => {
+app.post('/logout', (req, res) => {
     req.session.destroy(err => {
-
+        if (err) {
+            console.error('Session destruction error:', err);
+            return res.status(500).json({ error: 'Failed to log out' });
+        }
+        res.status(200).json({ message: 'Logged out successfully' });
+        
     })
 })
 
