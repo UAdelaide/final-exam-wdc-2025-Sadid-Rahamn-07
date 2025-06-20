3e079fdf57d2e8e600928c08d63120c1c09a33c2
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 19:30:58 +0000

    [automatic commit] 2025-06-20 19:30:58 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -74,7 +74,7 @@ app.post('/login', (req, res) => {
                 role: user.role
             };
             // checking user session cookies
-            console.log('Session user:' + req.session.user);
+            console.log('Session user:', req.session.user);
             // Remove password_hash from the user object before sending it back (security reasons)
             delete user.password_hash;
             res.status(200).json(
