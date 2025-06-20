414c071382e9831febc50d9e5e85243fc63ee0ce
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 14:35:59 +0000

    [automatic commit] 2025-06-20 14:35:59 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -6,19 +6,23 @@ require('dotenv').config();
 const app = express();
 
 //database connection
-mysql.createConnection({
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-})
-connect.connect((err) => {
-    if (err) {
-        console.error('Database connection failed:', err);
-    } else {
-        console.log('Connected to the database successfully');
-    }
 });
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err.stack);
+        return;
+    }
+    console.log('Connected to the database successfully');
+});
+
 
 // Middleware
 app.use(express.json());
