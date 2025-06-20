2fc3956a682455f818da4f44790b22dde4d462e0
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 20:27:41 +0000

    [automatic commit] 2025-06-20 20:27:41 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -106,9 +106,11 @@ app.get('load_user_dogs', (res, req) => {
     const username = req.session.user.username; // fetching username from session
     // SQL query to get all dogs for the user
     const query =
-    `
-    SELECT name
-                FROM Dogs WHERE owner_username = ?`;
+        `
+        SELECT name
+        FROM Dogs
+        INNER JOIN Users ON Dogs.owner_id = Users.id
+        WHERE owner_username = ?`;
     db.query(query, [username], (err, results) => {
         if (err) {
             console.error('Database query error:', err);
