32745add81da4e8749482d1d5078334baf4ca488
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 16:54:13 +0000

    [automatic commit] 2025-06-20 16:54:13 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -47,13 +47,14 @@ app.post('/login', (req, res) => {
     //  takes username and password from the request body from vue(fetch('/login')))
     const { username, password } = req.body;
     db.query(sql, [username, password], (err, results) => {
-      
+
         if (err) {
             console.error('Database query error:', err);
             return res.status(500).json({ error: 'Database query error' });
         }
         if (results.length > 0) {
             // User exists, send success response
+            user = results[0];
             req.session.user = {
                 username: user.username,
                 role: user.role
