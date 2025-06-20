87e51b7d35f2bcead75c8c227c35cccb7f285c5f
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 09:04:53 +0000

    [automatic commit] 2025-06-20 09:04:53 UTC 1.158.152.213

--- part1/app.js
+++ part1/app.js
@@ -59,6 +59,7 @@ app.get('/walkrequests/open', async (req, res) => {
             SELECT WalkRequests.request_id, Dogs.name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Dogs.owner_id
             FROM WalkRequests
             JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+            JOIN Users ON WalkRequests.dog_id = Dogs.dog_id
             WHERE status = "open"
         `);
         res.json(dogs_data);
