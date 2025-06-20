f399518320ad36c793c751cc7067aabff094fc20
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 14:35:39 +0000

    [automatic commit] 2025-06-20 14:35:39 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,8 @@ mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-}).connect((err) => {
+})
+connect.connect((err) => {
     if (err) {
         console.error('Database connection failed:', err);
     } else {
