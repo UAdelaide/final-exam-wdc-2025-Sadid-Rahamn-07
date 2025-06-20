943e28f0898b70c2ba03c8a953533d9f03097613
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 20:40:28 +0000

    [automatic commit] 2025-06-20 20:40:28 UTC 1.158.152.213

--- part2/app.js
+++ part2/app.js
@@ -110,7 +110,7 @@ app.get('load_user_dogs', (res, req) => {
         SELECT *
         FROM Dogs
         INNER JOIN Users ON Dogs.owner_id = Users.id
-        WHERE Users.username = ?`;
+        WHERE Users.owner_username = ?`;
     db.query(query, [username], (err, results) => {
         if (err) {
             console.error('Database query error:', err);
