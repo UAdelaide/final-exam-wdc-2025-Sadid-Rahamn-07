edf534fd7d84fac363465db03c4599309369ec2f
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-20 10:15:00 +0000

    [automatic commit] 2025-06-20 10:15:00 UTC 1.158.152.213

--- part1/app.js
+++ part1/app.js
@@ -83,18 +83,17 @@ app.get('/walkers/summary', async (req, res) => {
     try {
         const [walkers_summary] = await db.execute(`
             SELECT
-                    SELECT
-            WalkRequests.request_id,
-            Dogs.name AS dog_name,
-            WalkRequests.requested_time,
-            WalkRequests.duration_minutes,
-            WalkRequests.location,
-            Users.username AS owner_username
-        FROM WalkRequests
-        JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
-        JOIN Users ON Dogs.owner_id = Users.user_id
-        WHERE WalkRequests.status = 'open';
-            `);
+                WalkRequests.request_id,
+                Dogs.name AS dog_name,
+                WalkRequests.requested_time,
+                WalkRequests.duration_minutes,
+                WalkRequests.location,
+                Users.username AS owner_username
+            FROM WalkRequests
+            JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+            JOIN Users ON Dogs.owner_id = Users.user_id
+            WHERE WalkRequests.status = 'open';
+        `);
         res.json(walkers_summary);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch data' });
