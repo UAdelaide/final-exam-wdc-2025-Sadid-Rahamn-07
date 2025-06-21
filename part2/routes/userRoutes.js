59b4cd3d3159eed0f69fd0284e4998e234755fee
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 01:33:21 +0000

    [automatic commit] 2025-06-21 01:33:21 UTC 1.158.152.213

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
-
+require('dotenv').config();
 
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
