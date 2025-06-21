a4bbc6895df8a0126a8851f7d7a792c3b794029d
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 05:35:39 +0000

    [automatic commit] 2025-06-21 05:35:39 UTC 1.158.152.213

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -4,25 +4,9 @@ const db = require('../db');
 
 // Route to walkers summary as JSON
 
-router.get('/', async (req, res) => {
-    try {
-        const [walkers_data] = await db.execute(`
-            SELECT
-                u.username AS walker_username,
-                COUNT(DISTINCT wa.request_id) AS total_walks,
-                COUNT(DISTINCT r.rating_id) AS total_ratings,
-                ROUND(AVG(r.rating), 2) AS average_rating
-            FROM Users u
-            LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id
-            LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
-            WHERE u.role = 'walker'
-            GROUP BY u.user_id, u.username;
-        `);
-        res.json(walkers_data);
-    } catch (err) {
-        res.status(500).json({ error: 'Failed to fetch data' });
-    }
-}
+router.get('/', async (req, res) => {})
+
+
 
 router.get('/summary', async (req, res) => {
     try {
