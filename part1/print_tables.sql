fdc3351f9a423fad8532a5c4a7e3af12e3a6e4d3
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 06:46:25 +0000

    [automatic commit] 2025-06-21 06:46:25 UTC 1.158.152.213

--- part1/print_tables.sql
+++ part1/print_tables.sql
@@ -1,4 +1,8 @@
-SELECT wr.request_id FROM WalkRequests wr
-JOIN Dogs d ON wr.dog_id = d.dog_id
-WHERE d.name = 'Max' AND wr.status = 'completed'
-LIMIT 1;
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+VALUES (
+  (SELECT dog_id FROM Dogs WHERE name = 'Max' LIMIT 1),
+  '2025-06-15 08:00:00',
+  30,
+  'Central Park',
+  'completed'
+);
