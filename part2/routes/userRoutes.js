2d585df8e831ee12786e68739a9150fb75f58a72
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 01:53:29 +0000

    [automatic commit] 2025-06-21 01:53:29 UTC 1.158.152.213

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -81,10 +81,10 @@ router.post('/logout', (req, res) => {
 });
 
 // GET all dogs
-router.get('/dogs', (req, res) => {
+router.get('/dogs', async (req, res) => {
   // SQL query to get all dogs
   const dogs_sql = 'SELECT * FROM Dogs';
-  db.query(dogs_sql, (err, results) => {
+  await db.query(dogs_sql, (err, results) => {
     if (err) {
       console.error('Database query error:', err);
       return res.status(500).json({ error: 'Database query error' });
