ba9878fe3ccc10524ffea56edac03fc580ce8808
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 06:42:24 +0000

    [automatic commit] 2025-06-21 06:42:24 UTC 1.158.152.213

--- part1/print_tables.sql
+++ part1/print_tables.sql
@@ -1,6 +1,1 @@
-USE DogWalkService;
-SELECT * FROM Users;
-SELECT * FROM Dogs;
-SELECT * FROM WalkRequests;
-SELECT * FROM WalkApplications;
-SELECT * FROM WalkRatings;
+SELECT status, COUNT(*) FROM WalkRequests GROUP BY status;
