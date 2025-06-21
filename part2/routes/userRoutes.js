5d905e9738883559a47a592a548d2d208877c334
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 01:32:41 +0000

    [automatic commit] 2025-06-21 01:32:41 UTC 1.158.152.213

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -6,7 +6,6 @@ const router = express.Router();
 const db = require('../models/db');
 
 
-
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
   try {
@@ -16,7 +15,6 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch users' });
   }
 });
-
 
 // POST a new user (simple signup)
 router.post('/register', async (req, res) => {
@@ -45,6 +43,41 @@ router.get('/me', (req, res) => {
   });
 });
 
-
+// POST login (dummy version)
+router.post('/login', (req, res) => {
+  // SQL query to check if the user exists
+  const sql = 'SELECT * FROM Users WHERE username = ? AND password_hash = ?';
+  //  takes username and password from the request body from vue(fetch('/login')))
+  const { username, password } = req.body;
+  db.query(sql, [username, password], (err, results) => {
+    if (err) {
+      console.error('Database query error:', err);
+      return res.status(500).json({ error: 'Database query error' });
+    }
+    if (results.length > 0) {
+      // User exists, send success response
+      const user = results[0];
+      req.session.user = {
+        id: user.user_id,
+        username: user.username,
+        role: user.role
+      };
+      // checking user session cookies
+      console.log('Session user:', req.session.user);
+      // Remove password_hash from the user object before sending it back (security reasons)
+      delete user.password_hash;
+      res.status(200).json(
+        {
+          message: 'Successful',
+          user: results[0],
+          success: true
+        }
+      ); // Send the user data back
+    } else {
+      // User does not exist, send error response
+      res.status(401).json({ message: 'Failed', error: 'Invalid username or password' });
+    }
+  });
+});
 
 module.exports = router;
