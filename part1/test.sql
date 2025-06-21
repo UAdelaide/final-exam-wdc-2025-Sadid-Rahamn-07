5b5d9d307030c70f65eee9a364222d5c2a184195
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 05:57:47 +0000

    [automatic commit] 2025-06-21 05:57:47 UTC 1.158.152.213

--- part1/test.sql
+++ part1/test.sql
@@ -70,4 +70,4 @@ VALUES
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES
-((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 08:00:00', 30, 'Parklands', 'completed'),
+((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 08:00:00', 30, 'Parklands', 'completed')
