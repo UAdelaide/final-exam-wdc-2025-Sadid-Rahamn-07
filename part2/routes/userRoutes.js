aaf378c82e55b73fea3cbb314900a7b3b00b72e8
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 01:40:24 +0000

    [automatic commit] 2025-06-21 01:40:24 UTC 1.158.152.213

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -68,6 +68,18 @@ router.post('/login', async (req, res) => {
   });
 });
 
+// POST logout
+app.post('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Session destruction error:', err);
+            return res.status(500).json({ error: 'Failed to log out' });
+        }
+        res.status(200).json({ message: 'Logged out successfully' });
+
+    });
+});
+
 router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
