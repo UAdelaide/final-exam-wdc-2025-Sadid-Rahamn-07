d1d6f16eee1dc317b6d77d2cd7c90898aa4b77c4
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 06:02:28 +0000

    [automatic commit] 2025-06-21 06:02:28 UTC 1.158.152.213

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -15,14 +15,15 @@ router.get('/summary', async (req, res) => {
         const [walkers_summary] = await db.execute(`
         SELECT
             u.username AS walker_username,
-            COUNT(wa.rating) AS total_ratings,
-            AVG(wa.rating) AS average_rating,
-            COUNT(CASE WHEN wr.status = 'completed' THEN 1 END) AS completed_walks
-            FROM Users u
-            LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id
-            LEFT JOIN WalkRequests wr ON wa.request_id = wr.request_id
-            WHERE u.role = 'walker'
-            GROUP BY u.user_id, u.username;
+            COUNT(DISTINCT r.rating_id) AS total_ratings,
+            ROUND(AVG(r.rating), 2) AS average_rating,
+            COUNT(DISTINCT wa.status = 'accepted' AND wr.status = 'completed') AS completed_walks
+        FROM Users u
+        LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id
+        LEFT JOIN WalkRequests wr ON wa.request_id = wr.request_id
+        LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
+        WHERE u.role = 'walker'
+        GROUP BY u.user_id, u.username;
     `);
         res.json(walkers_summary);
     } catch (err) {
