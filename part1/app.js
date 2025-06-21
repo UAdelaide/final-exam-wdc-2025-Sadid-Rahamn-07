19d3d28bbd696d136b308318e271d3ae543b02dd
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 05:41:42 +0000

    [automatic commit] 2025-06-21 05:41:42 UTC 1.158.152.213

--- part1/app.js
+++ part1/app.js
@@ -5,39 +5,12 @@ var logger = require('morgan');
 var mysql = require('mysql2/promise');
 var app = express();
 
-// Middle   
+// Middleware
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-let db;
-
-(async () => {
-    try {
-        // Connect to MySQL without specifying a database
-        const connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '' // Set your MySQL root password
-        });
-
-        // Create the database if it doesn't exist
-        await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
-        await connection.end();
-
-        // Now connect to the created database
-        db = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'DogWalkService'
-        });
-    } catch (err) {
-        console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-    }
-})();
 
 
 const dogsRouter = require('./routes/dogs');
