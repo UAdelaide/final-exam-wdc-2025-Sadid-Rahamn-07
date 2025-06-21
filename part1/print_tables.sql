5b36a2f803ed34e34ef2889a566853a5024ddd96
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 06:44:25 +0000

    [automatic commit] 2025-06-21 06:44:25 UTC 1.158.152.213

--- part1/print_tables.sql
+++ part1/print_tables.sql
@@ -1,1 +1,5 @@
-SELECT status, COUNT(*) FROM WalkRequests GROUP BY status;
+SELECT wa.walker_id, u.username, wr.status, wa.status AS application_status, wr.request_id
+FROM WalkApplications wa
+JOIN Users u ON wa.walker_id = u.user_id
+JOIN WalkRequests wr ON wa.request_id = wr.request_id
+WHERE wr.status = 'completed';
