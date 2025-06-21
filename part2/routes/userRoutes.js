02d06686de440f903748a4ecd24cccedaa1768eb
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 01:23:37 +0000

    [automatic commit] 2025-06-21 01:23:37 UTC 1.158.152.213

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 });
 
 // POST login (dummy version)
-router.post('/login', (req, res) => {
+router.post('/', (req, res) => {
   console.log('Login route hit');
   // SQL query to check if the user exists
   const sql = 'SELECT * FROM Users WHERE username = ? AND password_hash = ?';
