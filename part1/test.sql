9c47122784f7aad8948080956b195b6fa8414ef0
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 05:54:46 +0000

    [automatic commit] 2025-06-21 05:54:46 UTC 1.158.152.213

--- part1/test.sql
+++ part1/test.sql
@@ -59,7 +59,7 @@ VALUES
 INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments, rated_at)
 VALUES
 (
-  (SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Max') LIMIT 1),
+  (SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Bella') LIMIT 1),
   (SELECT user_id FROM Users WHERE username = 'bobwalker'),
   (SELECT user_id FROM Users WHERE username = 'alice123'),
   4.5,
