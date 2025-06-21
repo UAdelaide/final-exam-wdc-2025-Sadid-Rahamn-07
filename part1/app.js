628d11eb8fd610a8bfd4fe2dabd0dadf8d02b79e
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 05:41:01 +0000

    [automatic commit] 2025-06-21 05:41:01 UTC 1.158.152.213

--- part1/app.js
+++ part1/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
-
+const db = require('../db');
 var app = express();
 
 //
