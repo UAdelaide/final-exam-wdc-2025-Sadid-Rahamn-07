a8e139b847ebac35a789d8ba49be3c78f3df373d
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 05:04:07 +0000

    [automatic commit] 2025-06-21 05:04:07 UTC 1.158.152.213

--- part1/routes/users.js
+++ part1/routes/users.js
@@ -23,4 +23,26 @@ router.get('/dogs', async (req, res) => {
     }
 });
 
+// Route to return walkrequests that are open as JSON
+app.get('/walkrequests/open', async (req, res) => {
+    try {
+        const [walkrequests_open] = await db.execute(`
+            SELECT
+                WalkRequests.request_id,
+                Dogs.name AS dog_name,
+                WalkRequests.requested_time,
+                WalkRequests.duration_minutes,
+                WalkRequests.location,
+                Users.username AS owner_username
+            FROM WalkRequests
+            JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+            JOIN Users ON Dogs.owner_id = Users.user_id
+            WHERE status = "open"
+        `);
+        res.json(walkrequests_open);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+});
+
 module.exports = router;
