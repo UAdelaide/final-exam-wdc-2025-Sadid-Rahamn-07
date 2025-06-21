8efb38ad85b3578fda554bd240d7ba0e44712940
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 05:06:08 +0000

    [automatic commit] 2025-06-21 05:06:08 UTC 1.158.152.213

--- part1/routes/users.js
+++ part1/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models/db'); // Import the database connection
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
