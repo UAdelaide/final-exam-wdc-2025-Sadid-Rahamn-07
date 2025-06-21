a47a6e059aacda44935f81a6e26eb9c000a9f25e
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 05:17:12 +0000

    [automatic commit] 2025-06-21 05:17:12 UTC 1.158.152.213

--- part1/routes/db.js
+++ part1/routes/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'DogWalkService',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'DogWalkService',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
