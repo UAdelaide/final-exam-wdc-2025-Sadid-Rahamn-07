e7b71e1656111193d2f0bafeef2acb839de9f35b
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 01:20:56 +0000

    [automatic commit] 2025-06-21 01:20:56 UTC 1.158.152.213

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -43,42 +43,6 @@ router.get('/me', (req, res) => {
   });
 });
 
-// POST login (dummy version)
-router.post('/login', (req, res) => {
-  console.log('Login route hit');
-  // SQL query to check if the user exists
-  const sql = 'SELECT * FROM Users WHERE username = ? AND password_hash = ?';
-  //  takes username and password from the request body from vue(fetch('/login')))
-  const { username, password } = req.body;
-  db.query(sql, [username, password], (err, results) => {
-    if (err) {
-      console.error('Database query error:', err);
-      return res.status(500).json({ error: 'Database query error' });
-    }
-    if (results.length > 0) {
-      // User exists, send success response
-      const user = results[0];
-      req.session.user = {
-        id: user.user_id,
-        username: user.username,
-        role: user.role
-      };
-      // checking user session cookies
-      console.log('Session user:', req.session.user);
-      // Remove password_hash from the user object before sending it back (security reasons)
-      delete user.password_hash;
-      res.status(200).json(
-        {
-          message: 'Successful',
-          user: results[0],
-          success: true
-        }
-      ); // Send the user data back
-    } else {
-      // User does not exist, send error response
-      res.status(401).json({ message: 'Failed', error: 'Invalid username or password' });
-    }
-  });
-});
+
 
 module.exports = router;
