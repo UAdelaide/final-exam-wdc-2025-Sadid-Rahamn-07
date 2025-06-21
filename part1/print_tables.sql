8005eee3eed1530c74d497fdc5f2500543900f4c
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 06:46:05 +0000

    [automatic commit] 2025-06-21 06:46:05 UTC 1.158.152.213

--- part1/print_tables.sql
+++ part1/print_tables.sql
@@ -1,9 +1,4 @@
-INSERT INTO WalkApplications (request_id, walker_id, status)
-VALUES (
-  (SELECT wr.request_id FROM WalkRequests wr
-    JOIN Dogs d ON wr.dog_id = d.dog_id
-    WHERE d.name = 'Max' AND wr.status = 'completed'
-    LIMIT 1),
-  (SELECT user_id FROM Users WHERE username = 'bobwalker' LIMIT 1),
-  'accepted'
-);
+SELECT wr.request_id FROM WalkRequests wr
+JOIN Dogs d ON wr.dog_id = d.dog_id
+WHERE d.name = 'Max' AND wr.status = 'completed'
+LIMIT 1;
