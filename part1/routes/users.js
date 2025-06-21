7bd0d7e21bbb28ba665609e7257055798fce718f
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-06-21 04:58:05 +0000

    [automatic commit] 2025-06-21 04:58:05 UTC 1.158.152.213

--- part1/routes/users.js
+++ part1/routes/users.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Route to return dogs as JSON
+router.get('/dogs', async (req, res) => {
+    try {
+        const [dogs_data] = await db.execute(`
+            SELECT
+                Dogs.name AS dog_name,
+                Dogs.size,
+                Users.username AS owner_username
+            FROM Dogs
+            JOIN Users ON Dogs.owner_id = Users.user_id
+        `);
+        res.json(dogs_data);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+});
+
 module.exports = router;
